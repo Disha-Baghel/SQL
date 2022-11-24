d1785cfcfdecc64118773ae5a6f0d0dbc1bd6a2b
Disha-Baghel <bagheldisha708@gmail.com>
2022-11-24 21:12:30 +0530

    Added new file

---
+++ assignment.sql
@@ -0,0 +1,6 @@
+create table countries (Name varchar2(10), 
+			area number(20), 
+			population number(20),
+			country_Code number(3),
+			Official_language varchar(20),
+			crime_rate number NULL);
